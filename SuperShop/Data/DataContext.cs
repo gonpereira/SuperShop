4fe82a768760682fab1eee67f5f26552fcf2c909
Goncalo Pereira <Goncalo.Pereira.22205@formandos.cinel.pt>
2021-12-16 16:29:00 +0000

    First Controller

---
+++ SuperShop/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperShop.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperShop.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DbSet<Product> Products { get; set; } //isto é a propriedade que vai fazer a tabela
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ SuperShop/Data/Entities/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SuperShop.Data.Entities
+{
+    public class Product
+    {
+
+        public int Id { get; set; } //detecta que é um id inteiro então cria-o como chave primaria tem de ser ID
+
+        //[Key]
+        //public int IdProduct { get; set; } //se quisesse que isto fosse chave primaria teria que usar o "[key]" a "DataAnnotations" transformar numa chave primaria
+
+        public string Name { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)] //formata isto com duas casas decimais em modo moeda mas em modo edicao não aplica formato nenhum
+        public decimal Price { get; set; }
+
+        [Display(Name = "Image")] //como este campo se vai chamar na página da web
+        public string ImageUrl { get; set; }
+
+        [Display(Name = "Last Purchase")]
+        public DateTime LastPurchase { get; set; }
+
+        [Display(Name = "Last Sale")]
+        public DateTime LastSale { get; set; }
+
+        [Display(Name = "Is Available")]
+        public bool IsAvailable { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
+        public double Stock { get; set; }
+
+    }
+}
