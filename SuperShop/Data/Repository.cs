a6567aef6d0eb144be4912c6dc0d7d9a865bf9a3
Goncalo Pereira <Goncalo.Pereira.22205@formandos.cinel.pt>
2021-12-20 19:54:42 +0000

    Create Repository

---
+++ SuperShop/Data/Repository.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SuperShop.Data.Entities;
+
+namespace SuperShop.Data
+{   
+    //Extract Interface
+    public class Repository : IRepository
+    {
+        private readonly DataContext _context;
+
+
+        //construtor
+        public Repository(DataContext context)
+        {
+            _context = context;
+        }
+
+        //metodo que mostra todos os produtos
+        public IEnumerable<Product> GetProducts()
+        {
+            return _context.Products.OrderBy(p => p.Name);
+        }
+
+        //metodo que mostra um só produto
+        public Product GetProduct(int id)
+        {
+            return _context.Products.Find(id);
+        }
+
+        public void AddProduct(Product product)
+        {
+            _context.Products.Add(product);
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _context.Products.Update(product);
+        }
+
+        public void RemoveProduct(Product product)
+        {
+            _context.Products.Remove(product);
+        }
+
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+            //Grava tudo o que estiver pendente na BD
+            //a entity framework é que faz esta gestão
+        }
+
+        //vai apenas saber se ele lá está... não precisa de ir buscar
+        public bool ProductExists(int id)
+        {
+            return _context.Products.Any(p => p.Id == id);
+        }
+    }
+}
---
+++ SuperShop/Data/SeedDb.cs
@@ -0,0 +1,48 @@
+﻿using SuperShop.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperShop.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context;
+
+        private Random _random;
+
+        public SeedDb(DataContext context)
+        {
+            _context = context;
+            _random = new Random();
+        }
+
+        public async Task SeedAsync()
+        {
+            await _context.Database.EnsureCreatedAsync(); //vai ver se a DB está criada se n tiver cria
+
+            if (!_context.Products.Any())
+            {
+                AddProduct("Iphone X");
+                AddProduct("Michey Mouse");
+                AddProduct("Iwatch Series 4");
+                AddProduct("Ipad Mini");
+                AddProduct("iphone 2");
+
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        private void AddProduct(string name)
+        {
+            _context.Products.Add(new Product
+            {
+                Name = name,
+                Price = _random.Next(1000),
+                IsAvailable = true,
+                Stock = _random.Next(100)
+            });
+        }
+    }
+}
